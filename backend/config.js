4fececd791fd414cd0cdecb55a8e455214a115c9
SrijaC2 <srijachanda06@gmail.com>
2024-10-01 02:29:02 +0530

    mernstack

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+// export const mongoDBURL = 'mongodb://localhost:27017'
+export const mongoDBURL = 'mongodb+srv://21951a66g0:sBOzhZOnvCr4dR3i@cluster0.xlcro.mongodb.net/'
